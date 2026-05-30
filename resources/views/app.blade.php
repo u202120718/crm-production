47241cc7e5d36496cca54064ee8f633aa59bcf1a
Andhre <u202120718@upc.edu.pe>
2026-05-30 17:21:28 -0500

    Actualizacion crm

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRM Solutions - Tecnologia Avanzada</title>
     <link rel="icon" type="image/jpg" href="{{ asset('logo.jpg') }}">
-
-    @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/app.jsx'])
 </head>
 <body>
